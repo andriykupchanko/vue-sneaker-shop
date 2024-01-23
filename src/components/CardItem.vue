f8d32f963de5ae8224a7c2c4d7bd1672b5bac8af
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-01-23 23:37:08 +0200

    basket

---
+++ src/components/CardItem.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <div class="flex items-center border border-slate-200 p-4 rounded-3xl gap-4">
+        <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneakers"/>
+        <div class="flex flex-col  justify-between">
+            <p class="text-sm">Sneakers Nike Air Max 270</p>
+            <div class="flex justify-between mt-2">
+                <b class="text-sm">250$</b>
+                <img class="opacity-40 hover:opacity-100 cursor-pointer" src="/close.svg" alt="close"/>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import DrawerHead from './DrawerHead.vue';
+    import CardItemList from './CardItemList.vue';
+</script>
+<template>
+    <div class="fixed top-0 left-0 h-full w-full bg-black 
+    z-10 opacity-70">
+        
+    </div>
+    <div class="bg-white w-96 h-full fixed top-0 right-0 
+    p-8 z-20"> 
+        <DrawerHead/>
+        <CardItemList/>
+        <div>
+            <div>
+                <span>
+
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
