33f2e5091752a5d0c7d83f7e8ed9767fee2c6134
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-01-27 23:58:29 +0200

    2

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { onMounted,reactive,ref, watch } from 'vue';
+import axios from 'axios';
+
+import Header from './components/Header.vue';
+import CardList from './components/CardList.vue';
+// import Drawer from './components/Drawer.vue';
+
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery : ''
+})
+const items = ref([]);
+
+const onChangeSelect = (event) =>{
+  filters.sortBy = event.target.value;
+}
+const onChangeInput = (event) =>{
+    filters.searchQuery = event.target.value;
+}
+const featchItems = async () =>{
+  try{
+    const params = {
+      sortBy:filters.sortBy
+    }
+    if(filters.searchQuery){
+      params.title =`*${filters.searchQuery}*`
+    }
+    const {data} = await axios.get (`https://fd8f5ac7362a2e05.mokky.dev/items`,{
+      params
+    })
+    items.value = data;
+  }catch(e){
+    alert(e.message);
+  }
+}
+
+onMounted(featchItems);
+watch(filters,featchItems);
+</script>
+
+<template>
+  <!-- <Drawer/> -->
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header/>
+    <div class="p-10 ">
+      <div class="flex flex-col  lg:flex-row gap-5 justify-between items-center">
+
+        <h2 class=" text-3xl font-bold ">All sneakers</h2>
+
+        <select @change="onChangeSelect" class="border py-2 pl-2 pr-3 rounded-md outline-none ">
+          <option value="name">By name</option>
+          <option value="price">By price (cheap)</option>
+          <option value="-price">By price (expensive)</option>
+        </select>
+
+        <div class="relative" >
+          <img class=" absolute left-4 top-3" src="/search.svg" alt="search"/>
+          <input 
+           @input="onChangeInput" 
+          type="text"
+          placeholder="Search..."
+          class="border rounded-md py-2 pl-11 pr-4 outline-none
+          focus:border-gray-400"/>
+        </div>
+        
+      </div>
+    </div>
+    <div class="mt-10">
+      <CardList :items="items"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Card from './Card.vue';
+defineProps({
+  items:Array
+})
+</script>
+<template>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        <Card  v-for="item in items" 
+          :key="item.id"
+          :imageUrl="item.imageUrl"
+          :title="item.title"
+          :price="item.price"
+        />
+      </div>
+      
+</template>
